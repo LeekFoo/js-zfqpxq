16e9592f6e5680d023ea01944ad0ce251a29bc67
LeekFoo <etaaniajp@infoseek.jp>
2019-11-08 06:57:24 +0000

    スキルセットの追加途中

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 AOS.init({
     offset: 300,
     duration: 1000,
+    once: true,
 });
 
 var options = {
@@ -26,41 +27,4 @@ var options = {
 $( document ).ready(function() {
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
-
-    var frontendChart = new Chart($('.frontend_graph'), {
-        type: 'radar',
-        data: {
-            labels: ['HTML/CSS', 'Javascript', 'jQuery', 'Angular', 'Bootstrap'],
-            datasets: [{
-                data: [4, 3, 4, 2, 2]
-            }]
-        },
-        options: options
-    });
-
-    options['title']['text'] = 'バックエンド';
-
-    var backendChart = new Chart($('.backend_graph'), {
-        type: 'radar',
-        data: {
-            labels: ['PHP', 'MySQL', 'PostgleSQL'],
-            datasets: [{
-                data: [4, 3, 4]
-            }]
-        },
-        options: options
-    });
-
-    options['title']['text'] = 'その他';
-
-    var otherChart = new Chart($('.other_graph'), {
-        type: 'radar',
-        data: {
-            labels: ['Linux', 'Git', 'Apache', 'AWS', 'Photoshop'],
-            datasets: [{
-                data: [4, 3, 4, 2, 2]
-            }]
-        },
-        options: options
-    });
 });
